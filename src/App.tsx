ec5c2cd63fb6105a4ed5311cdc28253c83113853
Christian Possidonio <christian.ornellas@calindra.com.br>
2020-05-21 13:23:35 -0300

    Ao clicar na teclaca M o menu é ativado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import Main from "./pages/Main"
 import Navigation from "./components/Navigation"
 import Slider from "./components/Slider"
@@ -9,6 +9,32 @@ const App: React.FC = () => {
 
     const handleEnterMenu = () => setShowMenu(true)
     const handleLeaveMenu = () => setShowMenu(false)
+
+    const useKey = (key: any, cb: any) => {
+        const callbackRef = useRef(cb)
+        useEffect(() => {
+            callbackRef.current = cb
+        })
+
+        useEffect(() => {
+            const handle = (event: any) => {
+                if (event.code === key) {
+                    callbackRef.current(event)
+                }
+            }
+            document.addEventListener("keypress", handle)
+            return () => document.removeEventListener("keypress", handle)
+        }, [key])
+    }
+
+    function handleKeyMenu() {
+        if (showMenu === true) {
+            setShowMenu(false)
+        } else {
+            setShowMenu(true)
+        }
+    }
+    useKey("KeyM", handleKeyMenu)
 
     return (
         <Container>
